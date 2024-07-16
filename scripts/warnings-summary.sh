63e568150a63c0aff4e876457ca23c26c4f5ee28
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2024-07-16 22:43:26 +0300

    scripts: warnings summary

--- scripts/warnings-summary.sh
+++ scripts/warnings-summary.sh
@@ -7,12 +7,29 @@ compilers=("$@");
   clang-{6.0,7,8,9,10,11,12,13,14,15,16,17,18,19}
   gcc-{7,8,9,10,11,12,13,14});
 
-printf -vh '%010d+%061d'; h="${h//0/-}";
+declare -i ncpu=$(nproc);
+((ncpu=ncpu<1 ?1 :ncpu));
+declare -i w=$(printf '%s\n' "${compilers[@]}" | wc -L);
+printf -vfmt '%%-%ds |%%%%3d %%%%s\\\\t%%%%s\\\\n' "$w";
+declare -i e=68-w++;
+printf -vh "%0${w}d+%0${e}d";
+h="${h//0/-}";
 
-echo "$h";
-for x in "${compilers[@]}"; do
-  cxx_="${x/#gcc/g}";
+e=0
+for cc_ in "${compilers[@]}"; do
+  cxx_="${cc_/#gcc/g}";
   cxx_="${cxx_/-/++-}";
-  y=$(eval make -j$(nproc) '-s -C src debug=1 CC='"'$x'"' CXX='"'$cxx_'"' '{'clean >/dev/null;'$'\n''make','-j1 2>&1 | grep -E "warning.+\[[-]W[^]]+\]" | sed -E "s/\x1b\[[0-9\;]*m//g;s/^([^:]*\/)?([^\/:]+):.+\[-W/\2\t[-W/" | sort | uniq -c | sort -h'});
-  (( ${#y} == 0 )) || { echo "$y" | sed "s/^/$(printf '%-9s |' "$x")/"; echo "$h"; };
+  make -j$ncpu -s -C src debug=1 CC="$cc_" CXX="$cxx_" clean >/dev/null 2>&1;
+  y=$(make -j1 -s -C src debug=1 CC="$cc_" CXX="$cxx_" 2>&1              |
+    grep -E "warning.+\[[-]W[^]]+\]"                                     |
+    sed -E "s/\x1b\[[0-9\;]*m//g;s/^([^:]*\/)?([^\/:]+):.+\[-W/\2\t[-W/" |
+    sort | uniq -c | sort -h);
+  (( ${#y} < 1 )) || {
+    printf -vfmt2 "$fmt" "$cc_";
+    printf -vout -- "$fmt2" $y
+    if (( e++ )); then
+      printf -- "%s$h\n" "$out";
+    else
+      printf -- "$h\n%s$h\n" "$out";
+    fi; };
 done
