f61d72804b80d529f6e5c27c3cc5384c33940398
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2024-07-29 00:04:35 +0300

    compat: add C23 fixed enum fallback

--- src/compat.h
+++ src/compat.h
@@ -15,6 +15,21 @@
 #  define nullptr NULL
 # endif /* clang < 16 || gcc < 13.1 */
 
+# if clang_older_than_version(8)  \
+  || gcc_older_than_version(13,1)
+#  define fixed_enum(name, T) enum name
+# elif clang_older_than_version(18)
+#  include "ligma.h"
+#  define fixed_enum(name, T)   \
+    diag_clang(push)            \
+    diag_clang(ignored          \
+      "-Wfixed-enum-extension") \
+    enum name : T               \
+    diag_clang(pop)
+# else
+#  define fixed_enum(name, T) enum name : T
+# endif /* clang < 8 || gcc < 13.1 */
+
 # if clang_older_than_version(19) \
   || gcc_older_than_version(13,1)
 #  define constexpr __attribute__((const))
