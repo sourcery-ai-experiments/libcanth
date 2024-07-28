c47089c3196ce5f1ee91a060e7ce7974f2300636
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2024-07-29 00:14:07 +0300

    compat: IntelliSense has skill issues

--- src/compat.h
+++ src/compat.h
@@ -16,7 +16,8 @@
 # endif /* clang < 16 || gcc < 13.1 */
 
 # if clang_older_than_version(8)  \
-  || gcc_older_than_version(13,1)
+  || gcc_older_than_version(13,1) \
+  || defined(__INTELLISENSE__)
 #  define fixed_enum(name, T) enum name
 # elif clang_older_than_version(18)
 #  include "ligma.h"
@@ -28,12 +29,13 @@
     diag_clang(pop)
 # else
 #  define fixed_enum(name, T) enum name : T
-# endif /* clang < 8 || gcc < 13.1 */
+# endif /* clang < 8 || gcc < 13.1 || __INTELLISENSE__ */
 
 # if clang_older_than_version(19) \
-  || gcc_older_than_version(13,1)
+  || gcc_older_than_version(13,1) \
+  || defined(__INTELLISENSE__)
 #  define constexpr __attribute__((const))
-# endif /* clang < 19 || gcc < 13.1 */
+# endif /* clang < 19 || gcc < 13.1 || __INTELLISENSE__ */
 #endif /* !__cplusplus */
 
 #ifndef __has_builtin
