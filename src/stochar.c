525cab0cfb6668cf67348a66c38f15857b1f1c96
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2024-08-26 23:53:30 +0300

    json: wip stochastic unicode 2

--- src/stochar.c
+++ src/stochar.c
@@ -61,6 +61,124 @@ static struct uz2 {
 	return ret;
 }
 
+union code_point {
+	uint32_t u32;
+	struct {
+		uint32_t b0 :  7;
+		uint32_t p0 : 25;
+	} utf32_0;
+	struct {
+		uint32_t b1 :  6;
+		uint32_t b0 :  5;
+		uint32_t p0 : 21;
+	} utf32_1;
+	struct {
+		uint32_t b2 :  6;
+		uint32_t b1 :  6;
+		uint32_t b0 :  4;
+		uint32_t p0 : 16;
+	} utf32_2;
+	struct {
+		uint32_t b3 :  6;
+		uint32_t b2 :  6;
+		uint32_t b1 :  6;
+		uint32_t b0 :  3;
+		uint32_t p0 : 11;
+	} utf32_3;
+
+	uint8_t u8[4];
+	struct {
+		uint32_t b0 :  7;
+		uint32_t p1 : 25;
+	} utf8_0;
+	struct {
+		uint32_t b0 :  5;
+		uint32_t p0 :  3;
+		uint32_t b1 :  6;
+		uint32_t p1 : 18;
+	} utf8_1;
+	struct {
+		uint32_t b0 :  4;
+		uint32_t p0 :  4;
+		uint32_t b1 :  6;
+		uint32_t p1 :  2;
+		uint32_t b2 :  6;
+		uint32_t p2 : 10;
+	} utf8_2;
+	struct {
+		uint32_t b0 :  3;
+		uint32_t p0 :  5;
+		uint32_t b1 :  6;
+		uint32_t p1 :  2;
+		uint32_t b2 :  6;
+		uint32_t p2 :  2;
+		uint32_t b3 :  6;
+		uint32_t p3 :  2;
+	} utf8_3;
+
+	unsigned char data[4];
+	char          str[4];
+};
+
+_Static_assert(sizeof(union code_point) * CHAR_BIT == 32U,"");
+
+#define BAD_CODE_POINT (constexpr union code_point){.u32 = UINT32_MAX}
+
+static const_inline bool
+code_point_error (const union code_point c)
+{
+	return c.u32 == BAD_CODE_POINT.u32;
+}
+
+static const_inline union code_point
+utf8_to_utf32 (const union code_point c,
+               const size_t           n)
+{
+	switch (n) {
+	case 1:
+		return (union code_point){
+			.utf32_0.b0 = c.utf8_0.b0,
+			.utf32_0.p0 = 0
+		};
+	case 2:
+		return (union code_point){
+			.utf32_1.b1 = c.utf8_1.b1,
+			.utf32_1.b0 = c.utf8_1.b0,
+			.utf32_1.p0 = 0
+		};
+	case 3:
+		return (union code_point){
+			.utf32_2.b2 = c.utf8_2.b2,
+			.utf32_2.b1 = c.utf8_2.b1,
+			.utf32_2.b0 = c.utf8_2.b0,
+			.utf32_2.p0 = 0
+		};
+	case 4:
+		return (union code_point){
+			.utf32_3.b3 = c.utf8_3.b3,
+			.utf32_3.b2 = c.utf8_3.b2,
+			.utf32_3.b1 = c.utf8_3.b1,
+			.utf32_3.b0 = c.utf8_3.b0,
+			.utf32_3.p0 = 0
+		};
+	default:
+		break;
+	}
+
+	return BAD_CODE_POINT;
+}
+
+static union code_point
+utf8_code_point (struct utf8 *const u8p)
+{
+	char const *const s = utf8_result(u8p);
+	return utf8_to_utf32(
+		(union code_point){
+			.str = {s[0], s[1], s[2], s[3]}
+		}, utf8_size(u8p)
+	);
+}
+
 int
 main (int    c,
       char **v)
@@ -89,9 +207,8 @@ main (int    c,
 		return EXIT_FAILURE;
 	}
 
-	size_t nr[4] = {0};
-	size_t n_bytes = 0;
 	struct utf8 u8p = utf8();
+	uint64_t n[] = {0,0,0,0};
 	char *out = buf;
 
 	for (int i = 0; i < letopt_nargs(&opt); ++i) {
@@ -112,12 +229,7 @@ main (int    c,
 				continue;
 			}
 
-			nr[utf8_size(&u8p) - 1U]++;
-			n_bytes += utf8_size(&u8p);
-
-			for (char const *s = utf8_result(&u8p); *s;) {
-				*out++ = *s++;
-			}
+			union code_point c = utf8_code_point(&u8p);
 		}
 
 		if (opt.m_separate) {
@@ -125,11 +237,12 @@ main (int    c,
 				*out++ = '\n';
 			*out = '\0';
 			out = buf;
-			(void)printf("%zu\t%zu\t%zu\t%zu\t%zu\t%s", nr[0],
-			             nr[1], nr[2], nr[3], n_bytes, buf);
+			(void)printf("%" PRIu64 "\t%" PRIu64 "\t%" PRIu64
+			             "\t%" PRIu64 "\t%" PRIu64 "\t%s",
+			             n[0], n[1], n[2], n[3], n[0] +
+			             2U*n[1] + 3U*n[2] + 4U*n[3], buf);
 
-			nr[0] = nr[1] = nr[2] = nr[3] = 0;
-			n_bytes = 0;
+			__builtin_memset(&n[0], 0, sizeof n);
 			utf8_reset(&u8p);
 		}
 	}
@@ -138,8 +251,10 @@ main (int    c,
 		if (out == buf || *(out - 1) != '\n')
 			*out++ = '\n';
 		*out = '\0';
-		(void)printf("%zu\t%zu\t%zu\t%zu\t%zu\t%s", nr[0],
-		             nr[1], nr[2], nr[3], n_bytes, buf);
+		(void)printf("%" PRIu64 "\t%" PRIu64 "\t%" PRIu64
+		             "\t%" PRIu64 "\t%" PRIu64 "\t%s",
+		             n[0], n[1], n[2], n[3], n[0] +
+		             2U*n[1] + 3U*n[2] + 4U*n[3], buf);
 	}
 
 	free(buf);
