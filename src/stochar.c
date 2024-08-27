159b498063557d9a4740d8e00787237179d7fdd4
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2024-08-28 00:16:33 +0300

    json: wip stochastic unicode 4

--- src/stochar.c
+++ src/stochar.c
@@ -46,23 +46,50 @@ saturated_add_uz (size_t a,
 	return a;
 }
 
-static struct uz2 {
-	size_t max;
-	size_t sum;
-} arg_sizes (struct letopt const *opt)
+static size_t
+arg_sizes_max (struct letopt const *opt)
 {
-	struct uz2 ret = {0};
-	for (int i = 0; i < letopt_nargs(opt); ++i) {
-		size_t n = strlen(letopt_arg(opt, i));
-		ret.sum = saturated_add_uz(ret.sum, n);
-		if (n > ret.max)
-			ret.max = n;
+	size_t ret = 0;
+	for (int i = 0, m = letopt_nargs(opt); i < m; ++i) {
+		size_t n = __builtin_strlen(letopt_arg(opt, i));
+		if (n > ret)
+			ret = n;
 	}
 	return ret;
 }
 
 /**
- * @brief Unicode code point encoding kind.
+ * @brief Calculate saturated sum of string argument lengths.
+ *
+ * The largest safe result is `SIZE_MAX - 1`. `SIZE_MAX` is always an
+ * error condition and indicates that an overflow would be inevitable
+ * if the result were to be used for string allocation.
+ *
+ * It is not possible to distinguish between an overflow and an actual
+ * `SIZE_MAX` sum. Such information would be useless, however, because
+ * either way there would be no space left for the null terminator.
+ *
+ * @param opt The option object whose string argument lengths to sum.
+ *
+ * @return The saturated sum of the argument string lengths.
+ * @retval `0` if the argument list is empty or all arguments are empty.
+ * @retval `SIZE_MAX` if the sum is greater than or equal to `SIZE_MAX`.
+ */
+static size_t
+arg_sizes_sum (struct letopt const *opt)
+{
+	size_t ret = 0;
+	for (int i = 0, m = letopt_nargs(opt); i < m; ++i) {
+		ret = saturated_add_uz(ret,
+		                       __builtin_strlen(letopt_arg(opt, i)));
+		if (ret == SIZE_MAX)
+			break;
+	}
+	return ret;
+}
+
+/**
+ * @brief Unicode code point encoding.
  *
  * These serve a dual purpose: they act as identifiers for different
  * encodings, and their enumeration values themselves are defined as
@@ -71,114 +98,148 @@ static struct uz2 {
  * maximum amount of data bits the corresponding code points use.
  */
 fixed_enum(ucp_kind, uint32_t) {
-	UCP_UTF8_1   = 1,
-	UCP_UTF8_2   = 2,
-	UCP_UTF8_3   = 3,
-	UCP_UTF8_4   = 4,
-	UCP_UTF32_7  = 7,
-	UCP_UTF32_11 = 11,
-	UCP_UTF32_16 = 16,
-	UCP_UTF32_21 = 21
+	UCP_UTF8_1   =  1, //!< UTF-8 encoding, 1 byte
+	UCP_UTF8_2   =  2, //!< UTF-8 encoding, 2 bytes
+	UCP_UTF8_3   =  3, //!< UTF-8 encoding, 3 bytes
+	UCP_UTF8_4   =  4, //!< UTF-8 encoding, 4 bytes
+	UCP_UTF32_7  =  7, //!< 1-byte UTF-8 as UTF-32
+	UCP_UTF32_11 = 11, //!< 2-byte UTF-8 as UTF-32
+	UCP_UTF32_16 = 16, //!< 3-byte UTF-8 as UTF-32
+	UCP_UTF32_21 = 21  //!< 4-byte UTF-8 as UTF-32
 };
 
-union code_point {
-	uint32_t u32;
-	struct {
-		uint32_t b0 :  7;
-		uint32_t p0 : 25;
-	} utf32_0;
-	struct {
-		uint32_t b1 :  6;
-		uint32_t b0 :  5;
-		uint32_t p0 : 21;
-	} utf32_1;
-	struct {
-		uint32_t b2 :  6;
-		uint32_t b1 :  6;
-		uint32_t b0 :  4;
-		uint32_t p0 : 16;
-	} utf32_2;
-	struct {
-		uint32_t b3 :  6;
-		uint32_t b2 :  6;
-		uint32_t b1 :  6;
-		uint32_t b0 :  3;
-		uint32_t p0 : 11;
-	} utf32_3;
-
-	uint8_t u8[4];
-	struct {
-		uint32_t b0 :  7;
-		uint32_t p1 : 25;
-	} utf8_0;
-	struct {
-		uint32_t b0 :  5;
-		uint32_t p0 :  3;
-		uint32_t b1 :  6;
-		uint32_t p1 : 18;
-	} utf8_1;
-	struct {
-		uint32_t b0 :  4;
-		uint32_t p0 :  4;
-		uint32_t b1 :  6;
-		uint32_t p1 :  2;
-		uint32_t b2 :  6;
-		uint32_t p2 : 10;
-	} utf8_2;
-	struct {
-		uint32_t b0 :  3;
-		uint32_t p0 :  5;
-		uint32_t b1 :  6;
-		uint32_t p1 :  2;
-		uint32_t b2 :  6;
-		uint32_t p2 :  2;
-		uint32_t b3 :  6;
-		uint32_t p3 :  2;
-	} utf8_3;
-
-	unsigned char data[4];
-	char          str[4];
+/**
+ * @brief 4-byte UTF-8 encoding.
+ */
+struct ucp_utf8_4 {
+	uint32_t b0 :  3; //!< Leading byte [ 0: 2]
+	uint32_t p0 :  5; //!<              [ 3: 7]
+	uint32_t b1 :  6; //!< Cont. byte 1 [ 8:13]
+	uint32_t p1 :  2; //!<              [14:15]
+	uint32_t b2 :  6; //!< Cont. byte 2 [16:21]
+	uint32_t p2 :  2; //!<              [22:23]
+	uint32_t b3 :  6; //!< Cont. byte 3 [24:29]
+	uint32_t p3 :  2; //!<              [30:31]
 };
 
-_Static_assert(sizeof(union code_point) * CHAR_BIT == 32U,"");
+/**
+ * @brief Unicode code point structure.
+ *
+ * Union-based type for representing Unicode code points in
+ * various encodings, moving data bits around between them,
+ * and for use as by-value function arguments.
+ */
+struct ucp {
+	union {
+		/** @brief Raw 32-bit data. */
+		uint32_t u32;
+
+		/** @brief UTF-32 conversion from 1-byte UTF-8. */
+		struct {
+			uint32_t b0 :  7; //!< ASCII byte   [ 0: 6]
+			uint32_t p0 : 25; //!< Padding      [ 7:31]
+		} utf32_0;
+
+		/** @brief UTF-32 conversion from 2-byte UTF-8. */
+		struct {
+			uint32_t b1 :  6; //!< Cont. byte 1 [ 0: 5]
+			uint32_t b0 :  5; //!< Leading byte [ 6:11]
+			uint32_t p0 : 21; //!< Padding      [12:31]
+		} utf32_1;
+
+		/** @brief UTF-32 conversion from 3-byte UTF-8. */
+		struct {
+			uint32_t b2 :  6; //!< Cont. byte 2 [ 0: 5]
+			uint32_t b1 :  6; //!< Cont. byte 1 [ 6:11]
+			uint32_t b0 :  4; //!< Leading byte [12:15]
+			uint32_t p0 : 16; //!< Padding      [16:31]
+		} utf32_2;
+
+		/** @brief UTF-32 conversion from 4-byte UTF-8. */
+		struct {
+			uint32_t b3 :  6; //!< Cont. byte 3 [ 0: 5]
+			uint32_t b2 :  6; //!< Cont. byte 2 [ 6:11]
+			uint32_t b1 :  6; //!< Cont. byte 1 [12:17]
+			uint32_t b0 :  3; //!< Leading byte [18:20]
+			uint32_t p0 : 11; //!< Padding      [21:31]
+		} utf32_3;
+
+		/** @brief Raw 8-bit data. */
+		uint8_t u8[4];
+
+		/** @brief Data bit accessor for 1-byte UTF-8 encoding. */
+		struct {
+			uint32_t b0 :  7; //!< ASCII byte   [ 0: 6]
+			uint32_t p1 : 25; //!<              [ 7:31]
+		} utf8_0;
+
+		/** @brief Data bit accessor for 2-byte UTF-8 encoding. */
+		struct {
+			uint32_t b0 :  5; //!< Leading byte [ 0: 4]
+			uint32_t p0 :  3; //!<              [ 5: 7]
+			uint32_t b1 :  6; //!< Cont. byte 1 [ 8:13]
+			uint32_t p1 : 18; //!<              [14:31]
+		} utf8_1;
+
+		/** @brief Data bit accessor for 3-byte UTF-8 encoding. */
+		struct {
+			uint32_t b0 :  4; //!< Leading byte [ 0: 3]
+			uint32_t p0 :  4; //!<              [ 4: 7]
+			uint32_t b1 :  6; //!< Cont. byte 1 [ 8:13]
+			uint32_t p1 :  2; //!<              [14:15]
+			uint32_t b2 :  6; //!< Cont. byte 2 [16:21]
+			uint32_t p2 : 10; //!<              [22:31]
+		} utf8_2;
+
+		struct ucp_utf8_4 utf8_3; //!< 4-byte UTF-8 encoding view.
+
+		/** @brief Raw byte data. */
+		unsigned char data[4];
+
+		/** @brief Raw character data. */
+		char          str[4];
+	}; //!< @anon
+};
+
+_Static_assert(sizeof(struct ucp) * CHAR_BIT == 32U,"");
 
 #if clang_at_least_version(19)
-# define BAD_CODE_POINT (const union code_point){.u32 = UINT32_MAX}
+# define BAD_CODE_POINT (const struct ucp){.u32 = UINT32_MAX}
 #else
-# define BAD_CODE_POINT (constexpr const union code_point){.u32 = UINT32_MAX}
+# define BAD_CODE_POINT (constexpr const struct ucp){.u32 = UINT32_MAX}
 #endif
 
 static const_inline bool
-code_point_error (const union code_point c)
+code_point_error (const struct ucp c)
 {
 	return c.u32 == BAD_CODE_POINT.u32;
 }
 
-static const_inline union code_point
-utf8_to_utf32 (const union code_point c,
-               const size_t           n)
+static const_inline struct ucp
+utf8_to_utf32 (const struct ucp c,
+               const size_t     n)
 {
 	switch (n) {
 	case 1:
-		return (union code_point){
+		return (struct ucp){
 			.utf32_0.b0 = c.utf8_0.b0,
 			.utf32_0.p0 = 0
 		};
 	case 2:
-		return (union code_point){
+		return (struct ucp){
 			.utf32_1.b1 = c.utf8_1.b1,
 			.utf32_1.b0 = c.utf8_1.b0,
 			.utf32_1.p0 = 0
 		};
 	case 3:
-		return (union code_point){
+		return (struct ucp){
 			.utf32_2.b2 = c.utf8_2.b2,
 			.utf32_2.b1 = c.utf8_2.b1,
 			.utf32_2.b0 = c.utf8_2.b0,
 			.utf32_2.p0 = 0
 		};
 	case 4:
-		return (union code_point){
+		return (struct ucp){
 			.utf32_3.b3 = c.utf8_3.b3,
 			.utf32_3.b2 = c.utf8_3.b2,
 			.utf32_3.b1 = c.utf8_3.b1,
@@ -192,12 +253,12 @@ utf8_to_utf32 (const union code_point c,
 	return BAD_CODE_POINT;
 }
 
-static union code_point
+static struct ucp
 utf8_code_point (struct utf8 *const u8p)
 {
 	uint8_t const *const d = (uint8_t const *)utf8_result(u8p);
 	return utf8_to_utf32(
-		(union code_point){.u8 = {d[0], d[1], d[2], d[3]}},
+		(struct ucp){.u8 = {d[0], d[1], d[2], d[3]}},
 		utf8_size(u8p)
 	);
 }
@@ -211,8 +272,8 @@ main (int    c,
 	if (letopt_nargs(&opt) < 1 || opt.m_help)
 		letopt_helpful_exit(&opt);
 
-	struct uz2 sz = arg_sizes(&opt);
-	size_t buf_sz = opt.m_separate ? sz.max : sz.sum;
+	size_t buf_sz = opt.m_separate ? arg_sizes_max(&opt)
+	                               : arg_sizes_sum(&opt);
 	buf_sz = saturated_add_uz(
 		saturated_add_uz(buf_sz, 2U),
 		saturated_add_uz(buf_sz, buf_sz));
@@ -232,7 +293,7 @@ main (int    c,
 
 	struct utf8 u8p = utf8();
 	uint64_t n[] = {0,0,0,0};
-	char *out = buf;
+	//char *out = buf;
 
 	for (int i = 0; i < letopt_nargs(&opt); ++i) {
 		uint8_t const *p = (uint8_t const *)letopt_arg(&opt, i);
@@ -240,9 +301,19 @@ main (int    c,
 			p = utf8_parse_next_code_point(&u8p, q);
 			if (u8p.error) {
 				if (opt.m_replace && (*p || opt.m_separate)) {
+					const struct ucp uc = utf8_to_utf32(
+						(struct ucp){
+							.u8 = {0xefU, 0xbfU,
+							       0xbdU, 0x00U}
+						},
+						3U
+					);
+					printf("U+%04" PRIx32 "\n", uc.u32);
+/*
 					*out++ = (char)(unsigned char)0xefU;
 					*out++ = (char)(unsigned char)0xbfU;
 					*out++ = (char)(unsigned char)0xbdU;
+*/
 				}
 				if (!*p)
 					break;
@@ -252,12 +323,13 @@ main (int    c,
 				continue;
 			}
 
-			const union code_point uc = utf8_code_point(&u8p);
+			const struct ucp uc = utf8_code_point(&u8p);
 			if (!code_point_error(uc))
 				printf("U+%04" PRIx32 "\n", uc.u32);
 		}
 
 		if (opt.m_separate) {
+/*
 			if (out == buf || *(out - 1) != '\n')
 				*out++ = '\n';
 			*out = '\0';
@@ -266,12 +338,12 @@ main (int    c,
 			             "\t%" PRIu64 "\t%" PRIu64 "\t%s",
 			             n[0], n[1], n[2], n[3], n[0] +
 			             2U*n[1] + 3U*n[2] + 4U*n[3], buf);
-
+*/
 			__builtin_memset(&n[0], 0, sizeof n);
 			utf8_reset(&u8p);
 		}
 	}
-
+/*
 	if (!opt.m_separate) {
 		if (out == buf || *(out - 1) != '\n')
 			*out++ = '\n';
@@ -281,7 +353,7 @@ main (int    c,
 		             n[0], n[1], n[2], n[3], n[0] +
 		             2U*n[1] + 3U*n[2] + 4U*n[3], buf);
 	}
-
+*/
 	free(buf);
 	return letopt_fini(&opt);
 }
